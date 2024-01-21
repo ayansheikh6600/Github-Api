8219dbf4c56e4b86199b19fb05378ce39e44a6ec
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-01-21 12:50:27 +0500

    first commit

---
+++ api/controller.ts
@@ -0,0 +1,59 @@
+const { gql, GraphQLClient } = require('graphql-request');
+
+interface PinnedRepos {
+    viewer: {
+      pinnedItems: {
+        nodes: {
+          name: string;
+          description: string;
+          url: string;
+          homepageUrl: string;
+          languages: {
+            nodes: {
+              name: string;
+              color: string;
+            }[];
+          };
+        }[];
+      };
+    };
+}
+
+export const getPinnedRepos = async () => {
+  const endpoint = 'https://api.github.com/graphql'
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+    },
+  })
+
+  const query = gql`
+    query {
+      viewer {
+        pinnedItems(first: 6, types: REPOSITORY) {
+          nodes {
+            ... on Repository {
+              name
+              description
+              url
+              homepageUrl
+              languages(first: 1) {
+                nodes {
+                  name
+                  color
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const data = await graphQLClient.request(query, {}) as PinnedRepos;
+    return data.viewer.pinnedItems.nodes;
+  } catch (error) {
+    console.error(error);
+  }
+}
